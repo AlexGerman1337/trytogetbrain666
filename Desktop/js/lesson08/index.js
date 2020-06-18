271f6848d58c3f5847fb6fac27c73996f6728aca
Alexander.German <questlogilt@mail.ru>
2020-06-18 13:20:05 +0300

    lesson09

---
+++ Desktop/js/lesson08/index.js
@@ -0,0 +1,40 @@
+
+
+
+console.log(document.querySelector('.salary-amount'));
+console.log(document.getElementById('start'));
+console.log(document.getElementsByTagName('button')[0]);
+console.log(document.getElementsByTagName('button')[1]);
+console.log(document.querySelector('.deposit-checkmark'));
+console.log(document.querySelectorAll('.additional_income-item'));
+console.log(document.querySelectorAll('.income-items'));
+console.log(document.querySelectorAll('.expenses-items'));
+console.log(document.querySelector('.additional_expenses-item'));
+console.log(document.querySelector('.target-amount'));
+console.log(document.querySelector('.period-select'));
+
+
+
+console.log(document.getElementsByClassName("budget_month-value"));
+console.log(document.getElementsByClassName("budget_day-value"));
+console.log(document.getElementsByClassName("expenses_month-value"));
+console.log(document.getElementsByClassName("additional_income-value"));
+console.log(document.getElementsByClassName("additional_expenses-value"));
+console.log(document.getElementsByClassName("income_period-value"));
+console.log(document.getElementsByClassName("target_month-value"));
+
+
+
+
+
+
+
+// Оставшиеся поля через querySelector каждый в отдельную переменную:
+
+// поля ввода (input) с левой стороны и не забудьте про range.
+
+
+
+
+// Проверить, чтобы все работало и не было ошибок в консоли
+
